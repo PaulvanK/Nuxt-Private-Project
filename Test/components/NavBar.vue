0f45a6e824c86e4c28ace9d2f21dc91ed7846ef9
PaulvanK <paulvk@jongstratransport.nl>
2023-06-09 14:01:34 +0200

    NavBar.vue added divs
    Added divs for enhanced customizability

--- Test/components/NavBar.vue
+++ Test/components/NavBar.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="body">
-    <div class="row">
-      <div class="btn btn-danger">Hallo</div>
+    <div class="navbar navbar-expand-lg navbar-css">
+      <div class="header">
+        <div class="row">
+          <div class="col text-start"></div>
+          <div class="col text-center"></div>
+          <div class="col text-end"></div>
+        </div>
+      </div>
     </div>
-    <div class="navbar">Look ma, no hands!</div>
+    <p>
+      Color palette:
+      <a href="https://colorhunt.co/palette/27374d526d829db2bfdde6ed"
+        >color palette site</a
+      >
+      with color codes 27374D (dark), 526D82 (a bit lighter), 9DB2BF (almost
+      lightest), DDE6ED (lightest color)
+    </p>
   </div>
 </template>
 
@@ -11,4 +24,10 @@
 export default {}
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+.navbar-css {
+  color: #f1efe9;
+  background-color: #27374d;
+  box-shadow: 0px 0px 15px 20px #27374d;
+}
+</style>
